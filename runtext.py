36e23ab2634ef3b8e0739bfcee763442e774eb44
mkwawama <79860095+mkwawama@users.noreply.github.com>
2024-01-29 16:09:05 +0000

    Commit runtext.py
    runtext.py is the python code that will display text on RGB Matrix screen.

---
+++ runtext.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# Display a runtext with double-buffering.
+from samplebase import SampleBase
+from rgbmatrix import graphics
+import time
+
+
+class RunText(SampleBase):
+    def __init__(self, *args, **kwargs):
+        super(RunText, self).__init__(*args, **kwargs)
+        self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
+        self.parser.add_argument("-R",  "--red",  help="Red value", default=255, type=int)
+        self.parser.add_argument("-G", "--green", help="Green value", default=255, type=int)
+        self.parser.add_argument("-B", "--blue", help="Blue value", default=0, type=int)
+
+    def run(self):
+        offscreen_canvas = self.matrix.CreateFrameCanvas()
+        font = graphics.Font()
+        font.LoadFont("../../../fonts/7x13.bdf")
+        red = self.args.red
+        green = self.args.green
+        blue = self.args.blue
+        textColor = graphics.Color(red, green, blue)
+        pos = offscreen_canvas.width
+        my_text = self.args.text
+
+        offscreen_canvas.Clear()
+        while True:
+            offscreen_canvas.Clear()
+            len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
+            pos -= 1
+            if (pos + len < 0):
+                pos = offscreen_canvas.width
+
+            time.sleep(0.05)
+            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+
+
+# Main function
+if __name__ == "__main__":
+    run_text = RunText()
+    if (not run_text.process()):
+        run_text.print_help()
